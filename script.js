94d23a42988bb65e65867d7f93abedf6d0feeb18
Anton <anton.vish2301@gmail.com>
2021-10-15 16:43:22 +0300

    практическое занятие 12

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let number = 5;
-const leftBorder = 1;
+// let number = 5;
+// const leftBorder = 1;
 
-number = 10;
-const obj = {
-    a:55,
+// number = 10;
+// const obj = {
+//     a:55,
+// };
+// obj.a = 10;
+// console.log(obj);
+
+
+//практический урок 12
+
+//1
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    pivate: false,
 };
-obj.a = 10;
-console.log(obj);
+
+// 3
+
+const a = prompt('Один из последних фильмов', ''),
+      b =  prompt('На сколько оцениет его?', ''), 
+      c =  prompt('Один из последних фильмов', ''), 
+      d =  prompt('На сколько оцениет его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies);
