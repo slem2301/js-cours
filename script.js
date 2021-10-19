e8ebd7946925e5660897b596f80160de7d3e1f7a
Anton <anton.vish2301@gmail.com>
2021-10-19 13:22:36 +0300

    урок 23 основы ооп

--- script.js
+++ script.js
@@ -62,68 +62,181 @@
 "ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных 
 genres
 P.S Функцию.
-*/ 
+*/
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
 
-start();
+// start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних фильмов', ''),
+//             b = prompt('На сколько оцениет его?', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
+// // rememberMyFilms();
+
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10 && personalMovieDB){
+//         console.log('просмотренно довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//         console.log('Вы классный зритель');
+//     } else if (personalMovieDB.count >= 30){
+//         console.log('Вы киноман');
+//     } else {
+//         console.log('Произошла ошибка');
+//     }
+// }
+// // detectPersonalLevel();
+
+// function showMyDB(hidden) {
+//     if(!hidden){
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for(let i = 1; i <= 3; i++){
+//         const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+//         personalMovieDB.genres[i-1] = genre;
+//     }
+// }
+// writeYourGenres();
+
+
+// //урок 20
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function () {   // метод объекта
+//         console.log("Test");
+//     }
+// };
+
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+
+
+
+//Урок 21 массивы
+
+// const arr = [32, 3, 44, 5, 27, 8,];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b){
+//     return a - b;
+// }
+
+// // arr.forEach(function(item, i, arr){
+// //     console.log(`${i}: ${item} внутри массива ${arr}`)
+// // });
+
+// // Урок 22 
+
+// const a = {
+//     x: 5,
+//     y: 7
+// };
+
+// const b = a; // копирование ссылки на объект, но не всего объекта
+
+// function copy(mainObj){
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 10,
+//     x: 20,
+//     c: {
+//         y: 40,
+//         z: 8
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 5;
+
+// console.log(newNumbers);
+// console.log(numbers);
+// // Это поверхностная копия объекта, не копирует вложенные объекты
+
+
+
+
+// Урок 23 основы ООП
+
+let str = 'some';
+let strObj = new String(str);
+
+// console.log(typeof(str));   строка 
+// console.log(typeof(strObj));   Объект
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello() {
+        console.log("Hello");
+    } 
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних фильмов', ''),
-            b = prompt('На сколько оцениет его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-// rememberMyFilms();
+// const john = {
+//     health: 100
+// };
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10 && personalMovieDB){
-        console.log('просмотренно довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log('Вы классный зритель');
-    } else if (personalMovieDB.count >= 30){
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-// detectPersonalLevel();
+const john = Object.create(soldier); // устанавливаем прототип в момент создания объекта
 
-function showMyDB(hidden) {
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
+// john.__proto__ = soldier; // это старый способ применения прототипов
 
-showMyDB(personalMovieDB.privat);
+// Object.setPrototypeOf(john, soldier); // первый аргумент которому хотим назначить прототип, второй - объект, прототип которого мы устанавливаем
 
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++){
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-        personalMovieDB.genres[i-1] = genre;
-    }
-}
-writeYourGenres();
+// console.log(john.armor);
+
+john.sayHello();
