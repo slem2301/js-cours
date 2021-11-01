08aeac72bddc949ef50f758016caef8a4b8b778d
Anton <anton.vish2301@gmail.com>
2021-11-01 13:46:52 +0300

    практика, урок 30

---
+++ practice/project/js/script.js
@@ -0,0 +1,56 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+
+const reclama = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+// 1)
+reclama.forEach(item => {
+    item.remove();
+});
+
+// 2)
+genre.textContent = 'драма';
+
+
+//3)
+poster.style.cssText = 'background: url(img/bg.jpg)  center center/cover no-repeat';
+
+// 4)
+movieList.innerHTML = "";
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">
+        ${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
